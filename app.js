c8467bbd7e594178af10cf2ba6870392c4414122
ffok123 <30338770+ffok123@users.noreply.github.com>
2022-09-28 11:57:38 +0800

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const html = `
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Hello from Render!</title>
+    <title>Hello from Fok!</title>
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
     <script>
       setTimeout(() => {
@@ -52,7 +52,7 @@ const html = `
   </head>
   <body>
     <section>
-      Hello from Render!
+      Hello from Florence!
     </section>
   </body>
 </html>
